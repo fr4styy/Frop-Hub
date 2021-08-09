e0df8e8c9e8800c7e17624c10fb3496dd6fd8d3c
Frost <76793505+frosty-frost@users.noreply.github.com>
2021-08-09 18:41:42 -0400

    Update init.client.lua

--- src/init.client.lua
+++ src/init.client.lua
@@ -1,1 +1,10 @@
-print('hi')
+local Games = require(script.GameList)
+print("start")
+for i,v in pairs(Games) do
+    print(i .. v)
+    if game.PlaceId == v then
+        print("test")
+        require(script.Games[i])
+    end
+end
+print("finish")
